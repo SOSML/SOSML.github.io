2e8e81ed72e037b510516fd63609501c95f5b1f3
Philip Wellnitz <philipwellnitz@gmx.de>
2019-10-12 18:39:16 +0900

    up

--- service-worker.js
+++ service-worker.js
@@ -27,24 +27,60 @@ self.addEventListener('message', (event) => {
  */
 self.__precacheManifest = [
   {
-    "url": "static/css/main.ce2b511f.css",
-    "revision": "2c5ce38fffe36ecf72185e6705e95740"
+    "url": "static/css/bootstrap-theme.css",
+    "revision": "b9b46bcc4dad6cc90fc4f95073c50735"
   },
   {
-    "url": "static/js/main.ab9f52a7.js",
-    "revision": "6a51ea0a1655e2c7b2b0d5ba0e904937"
+    "url": "static/css/bootstrap-theme.min.css",
+    "revision": "ab6b02efeaf178e0247b9504051472fb"
+  },
+  {
+    "url": "static/css/bootstrap.css",
+    "revision": "2a31dca112f26923b51676cb764c58d5"
+  },
+  {
+    "url": "static/css/bootstrap.min.css",
+    "revision": "ec3bb52a00e176a7181d454dffaea219"
+  },
+  {
+    "url": "static/css/main.3db3d947.css",
+    "revision": "24e0b167a103d8be091770bb4faea54c"
+  },
+  {
+    "url": "static/js/main.1b32d261.js",
+    "revision": "aeff1166387bfe26ec5bb92f868d9f43"
+  },
+  {
+    "url": "static/fonts/glyphicons-halflings-regular.eot",
+    "revision": "f4769f9bdb7466be65088239c12046d1"
+  },
+  {
+    "url": "static/fonts/glyphicons-halflings-regular.svg",
+    "revision": "89889688147bd7575d6327160d64e760"
+  },
+  {
+    "url": "static/fonts/glyphicons-halflings-regular.ttf",
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512"
+  },
+  {
+    "url": "static/fonts/glyphicons-halflings-regular.woff",
+    "revision": "fa2772327f55d8198301fdb8bcfc8158"
+  },
+  {
+    "url": "static/fonts/glyphicons-halflings-regular.woff2",
+    "revision": "448c34a56d699c29117adc64c43affeb"
   },
   {
     "url": "interpreter.js",
-    "revision": "6eb68391db7f3507a67728ff8fa86aee"
+    "revision": "1cb6d3c7a0bdbba76dd7944521eafa33"
   },
   {
     "url": "webworker.js",
-    "revision": "badde79962c4463a08d1a6ec2ad13ad9"
+    "revision": "fcf4111542c1d646eb6edc6f9344f445"
   },
   {
     "url": "index.html",
-    "revision": "def69f8c400de14589c67d2462a318bc"
+    "revision": "7bcb8fe3626ac10838146fd0be8097b2"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
